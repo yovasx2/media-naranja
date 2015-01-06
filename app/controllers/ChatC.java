5b105e3c3966bf6604baaed01f83ccb17e91ba40
yovasx2 <delirable@gmail.com>
2015-01-06 13:06:28 -0600

    project finished

--- app/controllers/ChatC.java
+++ app/controllers/ChatC.java
@@ -12,10 +12,14 @@ import play.mvc.WebSocket;
 import play.libs.F.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Arrays;
 
 public class ChatC extends Controller {
 
   private static List<WebSocket.Out<String>> sockets=new ArrayList<WebSocket.Out<String>>();
+  private static HashMap<WebSocket.Out<String>,String> users=new HashMap<WebSocket.Out<String>,String>();
+
 
   @Security.Authenticated(SecuredUser.class)
   public static Result chat() {
@@ -53,6 +57,41 @@ public class ChatC extends Controller {
           +"Por favor respeta a los demás usuarios.");
       }
     };
+  }
+
+  public static WebSocket<String> users() {
+    return new WebSocket<String>() {
+      public void onReady(WebSocket.In<String> in, final WebSocket.Out<String> out) {
+        // For each event received on the socket,
+        in.onMessage(new Callback<String>() {
+          public void invoke(String event) {
+            users.put(out,event);
+            sendNames();        
+          }
+        });
+
+        in.onClose(new Callback0(){
+          public void invoke(){
+            users.remove(out);
+            sendNames();
+          }
+        });
+
+      }
+    };
+  }
+
+  private static void sendNames(){
+    String names="";
+    Object[] usersArray=users.values().toArray();
+    Arrays.sort(usersArray);
+    for(Object user : usersArray){
+      names+=(String)user+"\n";
+    }
+
+    for(WebSocket.Out<String> out: users.keySet()){
+      out.write(names);
+    }
   }
 
 }
--- public/javascripts/chatIH.js
+++ public/javascripts/chatIH.js
@@ -1,22 +1,22 @@
+var messages;
+
 if(!window.WebSocket){
   alert("Tu navegador no puede hacer uso del chat porque no soporta WebSockets");
   window.location.href='/';
 }
+messages=new WebSocket('ws://'+window.location.host+'/message');
+messages.onmessage=function(evt) { 
+  output=document.getElementById("output");
+  output.innerHTML=output.innerHTML+evt.data+'\n';
+  output.scrollTop=output.scrollHeight;
+};
 
 function resetForm(){
   m=document.getElementById("message");
   m.value="";
 }
 
-var socket = new WebSocket('ws://'+window.location.host+'/message');
-socket.onmessage = function(evt) { 
-  var output=document.getElementById("output");
-  output.innerHTML=output.innerHTML+evt.data+'\n';
-  output.scrollTop = output.scrollHeight;
-};
-
-
-function submitForm(uri, user){
+function submitForm(uri, user, username){
   m=document.getElementById("message");
   container=document.getElementById("container");
   if(m.value==""){
@@ -25,12 +25,22 @@ function submitForm(uri, user){
     error.setAttribute("class","error");
     error.innerHTML='Escribe algo por favor';
     container.appendChild(error);
+    return;
   }else{
     error=document.getElementById("errorDD");
     if(error!=null)
       container.removeChild(error);
   }
   m=document.getElementById("message");
-  socket.send(user+m.value);
+  messages.send(user+m.value);
   m.value="";
 }
+
+$(document).ready(function() {
+  $('.submitOnEnter').keydown(function(event) {
+    if (event.keyCode == 13) {
+      document.getElementById('submitButton').click();
+      return false;
+    }
+  });
+});
